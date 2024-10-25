b9e14f3178591bfcd7674d2a858b464007b78523
jabezmageto <164736234+jabezmageto@users.noreply.github.com>
2024-10-25 07:35:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_j37m3e9","template_dmh73ua",parms).then(alert("Email Sent !!"))
+}
